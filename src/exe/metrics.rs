243829206d608fe140716078d5b54f9027b2a918
dimitrmo <dmouratidis153@gmail.com>
2023-04-09 12:44:20 +0300

    Rename

--- src/exe/metrics.rs
+++ src/exe/metrics.rs
@@ -2,7 +2,9 @@ use lazy_static::lazy_static;
 use prometheus::{register_gauge, Gauge};
 
 lazy_static! {
-    pub static ref TOTAL_CHANNELS: Gauge =
-        register_gauge!("total_channels", "Total number of active channels.",)
-            .expect("cannot create gauge metric for channels");
+    pub static ref TOTAL_CHANNELS: Gauge = register_gauge!(
+        "total_producer_channels",
+        "Total number of active channels.",
+    )
+    .expect("cannot create gauge metric for channels");
 }
